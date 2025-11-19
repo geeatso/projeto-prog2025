f102c161852ee30927ca85a2bbb072b03cf02cc3
Geeatso <100881879+geeatso@users.noreply.github.com>
2025-11-18 23:28:16 -0300

    função pentágono
    funcional, mas o código assume que o valor dos lados é igual.

---
+++ ufpr2.py
@@ -0,0 +1,76 @@
+import math
+
+### FUNÇÃO TRIÂNGULOS
+
+def triangulo(lados):
+    a, b, c = lados
+    
+# Classificação do triângulo (equilátero, escaleno ou isósceles)
+
+    if a == b:
+        if b == c:
+            classificacao = "Triângulo Equilátero"
+        else:
+            classificacao = "Triângulo Isósceles"
+    else:
+        if a == c:
+            classificacao = "Triângulo Isósceles"
+        else:
+            if b == c:
+                classificacao = "Triângulo Isósceles"
+            else:
+                classificacao = "Triângulo Escaleno"
+    
+# Cálculo de perímetro e área
+    perimetro = a + b + c
+    s = perimetro/2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    
+    return classificacao, area, perimetro
+
+### FUNÇÃO QUADRILÁTEROS
+
+# Cálculo de perímetro e área
+
+def quadrilatero(lados):
+    a, b, c, d = lados
+    perimetro = a + b + c + d
+    area = a * b
+    return "Quadrilátero", area, perimetro
+    
+### FUNÇÃO PENTÁGONO
+
+def pentagono(lados):
+    lado = lados[0]
+    
+# Cálculo de perímetro e área
+    
+    perimetro = sum(lados)
+    area = (1/4) * math.sqrt(5 * (5 + 2 * math.sqrt(5))) * lado * lado
+    
+    return "Pentágono", area, perimetro
+
+### Input único (obrigatório)
+
+entrada = input("Digite os números desejados separados por vírgula: ")
+lista = [float(digito) for digito in entrada.split(',')]
+
+### Classificação e resultado final
+
+if len(lista) == 3:
+    classificacao, area, perimetro = triangulo(lista)
+    print("Forma geométrica:", classificacao)
+    print("Perímetro:", round(perimetro))
+    print("Área:", round(area))
+else:
+    if len(lista) == 4:
+        classificacao, area, perimetro = quadrilatero(lista)
+        print("Forma geométrica:", classificacao)
+        print("Perímetro:", round(perimetro))
+        print("Área:", round(area))
+    else:
+        if len(lista) == 5:
+            classificacao, area, perimetro = pentagono(lista)
+            print("Forma geométrica:",classificacao)
+            print("Perímetro:", round(perimetro))
+            print("Área:", round(area))
